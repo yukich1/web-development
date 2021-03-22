d2971fc79eb6c98c479a8f14c8bf0e60f9957e49
yukich1 <79829721+yukich1@users.noreply.github.com>
2021-03-22 13:43:07 +0300

    Update task2.php

--- lw3/task2.php
+++ lw3/task2.php
@@ -1,23 +1,24 @@
 <?php
+  if (!$_GET['text'])
+  {
+    echo "Incorrected parametr";
+    return;
+  }
+
   $string = $_GET['text'];
-  $first = $string[0];
-  $second = $string[1];
-  if ($string <> 0)
-    {
-      if (ctype_digit($first))
-        {
-        if ($second <> 0)
-          {
-    	      echo "No can't start on number";
-          }
-        }
-        else
-         {
-           echo "If only one symbol then it's can't be a number";
-         }
-      else
-      {
-        echo "Yes";
-      }
-    }
-?> 
+  $firstChar = $string[0];
+  if (!ctype_alnum($string))
+  {
+    echo "Invalid string";
+    return;
+  }
+
+  if (ctype_digit($firstChar))
+  {
+    echo "No, can't start on number";
+  }
+  else
+  {
+    echo "Yes";
+  }
+?>
